88e7b4d69b4f02d16150e9e09625dc2d3390a3d3
Samsonite <sam.demarco@gmail.com>
2022-12-06 10:05:04 -0500

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,14 @@
 
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const jobBoardRoute = require('./jobBoardRoute');
+
+
+router.use('/', homeRoutes);
+router.use('/job_board', jobBoardRoute);
+
+router.use('/api', apiRoutes);
+
+module.exports = router;
