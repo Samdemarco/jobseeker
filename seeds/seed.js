339165fc730428633b6e596d88f292fb562b11c3
Samsonite <sam.demarco@gmail.com>
2022-12-08 18:55:00 -0500

    Added 'Jobs' table Sequelize commands to create table

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,29 @@
 const sequelize = require('../config/connection');
-const { User, Job } = require('../models');
+const Jobs = require('../models/Jobs');
+const  User   = require('../models/User');
 
 const userData = require('./userData.json');
+const jobData = require('./jobData.json');
+
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  console.log("userData: " + userData);  
+  console.log("jobData: " + jobData);  
+
 
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
+     await Jobs.bulkCreate(jobData);
+
   process.exit(0);
 };
 
 seedDatabase();
+
+
+
+
