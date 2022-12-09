b2ec3fe16da9b12b2b8ea811124bf2d6073a9771
mbenitez <mbenitez.a@gmail.com>
2022-12-09 16:57:41 -0500

    Fix jobBoardRoute to display all jobs

--- controllers/jobBoardRoute.js
+++ controllers/jobBoardRoute.js
@@ -1,22 +1,21 @@
 const router = require('express').Router();
-const   User   = require('../models/User');
+const { Job } = require('../models');
 
+// Get all jobs in the database
 router.get('/', async (req, res) => {
   try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    // Get all jobs, sorted by title
+    const jobData = await Job.findAll({
+      attributes: { exclude: ['id'] },
+      order: [['job_title', 'ASC']],
     });
 
     // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
+    const jobs = jobData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data into Handlebars.js template
-    console.log(users); 
-
-    res.render('job_board', {users} );
-    console.log(users); 
+    res.render('job_board', {jobs} );
+    //console.log(jobs); 
 
   } catch (err) {
     res.status(500).json(err);
