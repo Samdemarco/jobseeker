340a64c6a3d38ae9aebbcb900c27ea0c1b3c648d
mbenitez <mbenitez.a@gmail.com>
2022-12-08 20:31:30 -0500

    Rename Jobs.js to Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,39 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
 
 class Job extends Model {
-  checkPassword(loginPassword) {
-    return bcrypt.compareSync(loginPassword, this.password);
-  }
+ 
 }
 
 Job.init(
   {
-    job_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    job_URL: {
+    job_title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },   
+    job_city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
+    employer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
   },
-{
-  hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-    },
+  {   
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'job',
-}
+  }
 );
 
 module.exports = Job;
