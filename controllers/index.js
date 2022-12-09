7313a797212e6ad0792cfa8f540c90c6c291c564
Samsonite <sam.demarco@gmail.com>
2022-12-08 19:20:15 -0500

    Latest index.js for controllers with added jobBoardRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,13 @@
-
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const jobBoardRoute = require('./jobBoardRoute');
+const userRoutes = require('./userRoutes');
+
 
 
 router.use('/', homeRoutes);
 router.use('/job_board', jobBoardRoute);
-
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
