de6c191f77e6cb97fa67dc629f93f4c72b55dfb6
mbenitez <mbenitez.a@gmail.com>
2022-12-09 21:17:00 -0500

    Update file to render login page, start session implementation

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,11 @@
-
 const router = require('express').Router();
 
-//const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
 const jobBoardRoute = require('./jobBoardRoute');
 
-
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 router.use('/job_board', jobBoardRoute);
-
-//router.use('/api', apiRoutes);
 
 module.exports = router;
