e378efb3b2127781ab1cfd656449d22911fc8df4
mbenitez <mbenitez.a@gmail.com>
2022-12-12 16:14:21 -0500

    Fix typo in userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -84,7 +84,7 @@ router.post('/login', async (req, res) => {
 // Logout
 router.post('/logout', (req, res) => {
   // When the user logs out, destroy the session
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
